cfc57a63ca8a90b3f5fd630848c27f45e88ec892
TorstenWeber <TorstenWeber12@gmail.com>
2014-01-31 13:34:55 +0100

    Added documentation

--- feyn.h
+++ feyn.h
@@ -5,9 +5,32 @@ Vectors p;
 
 CFunction denom; 
 
-* QED
+*   ___  _____ ____
+*  / _ \| ____|  _ \ 
+* | | | |  _| | | | |
+* | |_| | |___| |_| |
+*  \__\_\_____|____/ 
+*
+
 Function fermionIn, fermionOut, antiFermionIn, antiFermionOut, photon;
-Functions fermionPropagator, photonPropagator, QEDVertex;
+
+* fermionPropagator(Index i, Vector p, Symbol m)
+*   i: Index for the gamma trace (as in g_(i, ...))
+*   p: Momentum of the fermion
+*   m: Mass of the fermion
+Function fermionPropagator;
+
+* photonPropagator(Vector p, Symbol z, Index mu, Index nu)
+*   p: Momentum of the photon
+*   z: gauge zeta
+*   mu, nu: Lorentz-indices of the propagator
+Function photonPropagator
+
+* QEDVertex(Index i, Symbol c, Index mu)
+*   i: Index for the gamma trace (as in g_(i, ...))
+*   c: Charge of the fermion
+*   mu: Lorentz-index of the vertex
+Function QEDVertex;
 
 * vim:ft=form
 #+
